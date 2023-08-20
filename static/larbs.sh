d2e009abeae4c357661d0968f16faf0b943e97b1
ricardobossan <ricardobossan@protonmail.com>
2023-08-20 13:00:45 -0300

    Uses ricardobossan's config files, based off of Luke's config

--- static/larbs.sh
+++ static/larbs.sh
@@ -6,7 +6,7 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
+dotfilesrepo="https://github.com/ricardobossan/voidrice.git"
 progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
